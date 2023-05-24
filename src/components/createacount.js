9833482358e765091a4a2efaa37a3b132cbb315f
pmdevika <98165952+pmdevika@users.noreply.github.com>
2023-05-25 00:23:32 +0530

    Update createacount.js

--- src/components/createacount.js
+++ src/components/createacount.js
@@ -1,29 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/home.css';
 import { Link } from 'react-router-dom';
 
-const createacount = () => {
+const CreateAccount = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Display the entered values on the console
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+
+    fetch("http://localhost:3002/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("Registration Successful");
+        } else {
+          alert("Something went wrong");
+        }
+      });
+  };
+
   return (
     <div>
-       <div id="login1">
+      <div id="login1">
         <div className="loginpage">
           <div className="loginframe">
             <h1 className='h'>Create Account</h1>
-            <input type="text" className="input1" placeholder="username" />
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="input1"
+                placeholder="Username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
               <br />
-              <input type="text" className="input1" placeholder="password" />
+              <input
+                type="password"
+                className="input1"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
               <br />
-              <input type="text" className="input1" placeholder="confirm password" />
+              <input
+                type="password"
+                className="input1"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
               <br />
-              
-             <Link to="/profile"> <button type="submit" className="button">Submit</button></Link>
+              <button type="submit" className="button">
+                Submit
+              </button>
               <br />
-            </div>
-            </div>
-            </div>
-            
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default createacount;
+export default CreateAccount;
